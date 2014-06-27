a646d05021eb1d4be80463354e70ca19f897fdb1
Nate Armstrong <nate@custombit.com>
2014-06-27 16:05:52 -0600

    Adds user method for calculating tasks complete %

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true
 	has_secure_password
+
+  def tasks_completed_percent
+    return 0.0 if tasks.empty?
+    (tasks.completed.count / tasks.count) * 100
+  end
 end
